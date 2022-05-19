eea664cbef0676a36398a91434f7bae9d0fe964e
CristianN998 <cristiancasallas998@gmail.com>
2022-05-18 21:36:11 -0500

    web speech project working on

--- my-js-course/web-speech/handlers.js
+++ my-js-course/web-speech/handlers.js
@@ -1,24 +1,47 @@
-import { isValidColor } from './colors';
+import wait from 'waait';
+import { isValidColor, isDark } from './colors';
 
+let scoreSum = 0;
 function logWords(results) {
-  // console.log(results[results.length - 1][0].transcript);
+  console.log(results[results.length - 1][0].transcript);
 }
-
-export function handleResult({ results }) {
+export async function handleResult({ results }) {
   logWords(results);
   const words = results[results.length - 1][0].transcript;
+  console.log(words);
   // lowercase everything
   let color = words.toLowerCase();
   // strip any spaces out
   color = color.replace(/\s/g, '');
-  // check if it is a valid colour
-  if (!isValidColor(color)) return; // thats all folks
+  // check if it is a valid colours
+  if (!isValidColor(color)) return;
   // if it is, then show the UI for that
-  const colorSpan = document.querySelector(`.${color}`);
+  const colorSpan = document.getElementById(`${color}`);
+  const score = document.querySelector('.score');
+  const title = document.querySelector('.title');
   colorSpan.classList.add('got');
   console.log(colorSpan);
   console.log('This is a valid color!');
   console.log(color);
-  // change the background color
-  document.body.style.backgroundColor = color;
+  // change color background
+  document.body.style.background = `${color}`;
+  console.log(isDark(color));
+  if (isDark(color) === true) {
+    score.classList.add('light');
+    score.classList.remove('dark');
+    title.classList.add('light');
+    title.classList.remove('dark');
+  } else {
+    score.classList.add('dark');
+    score.classList.remove('light');
+    title.classList.add('dark');
+    title.classList.remove('light');
+  }
+
+  await wait(350);
+  score.textContent = `Score: ${(scoreSum += 1)}`;
+  colorSpan.classList.add('none');
 }
+// high score keep score in local storage
+// look in all of the words split each word and then loop in all of the words if it is
+// remove all of the colors
--- my-js-course/web-speech/speech.js
+++ my-js-course/web-speech/speech.js
@@ -1,36 +1,34 @@
-import { handleResult } from './handlers';
-import { colorsByLength, isDark } from './colors';
+import { handleResult } from './handlers.js';
+import { colorsByLength, isDark } from './colors.js';
 
 const colorsEl = document.querySelector('.colors');
-
 function displayColors(colors) {
   return colors
     .map(
-      color =>
-        `<span class="color ${color} ${
-          isDark(color) ? 'dark' : ''
-        }" style="background: ${color};">${color}</span>`
+      (color) =>
+        `<span class="color ${isDark(color) ? 'dark' : ''}"
+        id="${color}"
+         style="background:${color}">${color}</span>`
     )
     .join('');
 }
 
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
-
 function start() {
-  // see if their browser supports this
+  // see if their browsers supports this;
   if (!('SpeechRecognition' in window)) {
-    console.log('Sorry your browser does not support speech reco. ');
-    return;
+    console.log('sorry your browser does not supportSpeechRecognition');
   }
   // it does work
-  console.log('Starting...');
-  // make a new speech reco
+  console.log('starting...');
+  // make a new speech recognition
   const recognition = new SpeechRecognition();
   recognition.continuous = true;
   recognition.interimResults = true;
   recognition.onresult = handleResult;
   recognition.start();
+  console.log(recognition);
 }
 
 start();

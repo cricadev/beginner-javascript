9e1f1b4f4f99913b98c48f2ae269f4d9fe364bce
CristianN998 <cristiancasallas998@gmail.com>
2022-06-01 15:46:22 -0500

    learned about es6 modules but didnt udnerstand about webpack XD

---
+++ es6-course/13-js-modules/es6modules/dist/app.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _lodash = require("lodash");
+
+var _insane = _interopRequireDefault(require("insane"));
+
+var _jsonp = _interopRequireDefault(require("jsonp"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
---
+++ es6-course/13-js-modules/es6modules/dist/webpack.config.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'production',
+  devtool: 'source-map',
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js'
+  }
+};
